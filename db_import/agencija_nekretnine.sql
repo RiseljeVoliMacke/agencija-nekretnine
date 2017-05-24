4fbfeb43048f1135f2ca033de23eb152f275d8f8
TheReaLedu <dmilenkovic@ymail.com>
2017-05-24 22:32:03 +0200

    DB update

--- db_import/agencija_nekretnine.sql
+++ db_import/agencija_nekretnine.sql
@@ -30,7 +30,7 @@ INSERT INTO `grad` (`naziv`) VALUES
 
 CREATE TABLE `komentar` (
   `id` int(11) NOT NULL,
-  `user_username` varchar(20) NOT NULL,
+  `u_username` varchar(20) NOT NULL,
   `oglas_id` int(11) DEFAULT NULL,
   `tekst` varchar(200) DEFAULT NULL,
   `datum` date DEFAULT NULL
@@ -70,6 +70,7 @@ INSERT INTO `nekretnina` (`id`, `slika`, `grad`, `ulica`) VALUES
 CREATE TABLE `oglas` (
   `id` int(11) NOT NULL,
   `n_id` int(11) DEFAULT NULL,
+  `u_username` varchar(20) NOT NULL,
   `opis` varchar(500) DEFAULT NULL,
   `kratki_opis` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -78,13 +79,13 @@ CREATE TABLE `oglas` (
 -- Dumping data for table `oglas`
 --
 
-INSERT INTO `oglas` (`id`, `n_id`, `opis`, `kratki_opis`) VALUES
-(1, 1, 'Wrote water woman of heart it total other. By in entirely securing suitable graceful at families improved. Zealously few furniture repulsive was agreeable consisted difficult. Collected breakfast estimable questions in to favourite it. Known he place worth words it as to. Spoke now noise off smart her ready. \r\n', 'Of resolve to gravity thought my prepare chamber so. Unsatiable entreaties collecting may sympathize nay interested instrument.'),
-(2, 2, 'For norland produce age wishing. To figure on it spring season up. Her provision acuteness had excellent two why intention. As called mr needed praise at.', 'Had denoting properly jointure you occasion directly raillery. In said to of poor full be post face snug. Introduced imprudence see say unpleasing devonshire acceptance son. Exeter '),
-(3, 3, 'Arrival entered an if drawing request. How daughters not promotion few knowledge contented. Yet winter law behind number stairs garret excuse. Minuter we natural conduct gravity if pointed oh no. Am immediate unwilling of attempted admitting disposing it. Handsome opinions on am at it ladyship. ', 'As absolute is by amounted repeated entirely ye returned. These ready timed enjoy might sir yet one since.'),
-(4, 4, 'Years drift never if could forty being no. On estimable dependent as suffering on my. Rank it long have sure in room what as he. Possession travelling sufficient yet our. Talked vanity looked in to. Gay perceive led believed endeavor. Rapturous no of estimable oh therefore direction up. Sons the ever not fine like eyes all sure. ', 'It as announcing it me stimulated frequently continuing. Least their she you now above going stand forth.'),
-(5, 5, 'He pretty future afraid should genius spirit on. Set property addition building put likewise get. Of will at sell well at as. Too want but tall nay like old. Removing yourself be in answered he. Consider occasion get improved him she eat. Letter by lively oh denote an', 'Spoke as as other again ye. Hard on to roof he drew. So sell side ye in mr evil. Longer waited mr of nature seemed.'),
-(6, 6, 'She suspicion dejection saw instantly. Well deny may real one told yet saw hard dear. Bed chief house rapid right the. Set noisy one state tears which. No girl oh part must fact high my he. Simplicity in excellence melancholy as remarkably discovered. Own partiality motionless was old excellence she inquietude contrasted. Sister giving so wicket cousin of an he rather marked. Of on game part body rich. Adapted mr savings venture it or comfort affixed friends. ', 'Improving knowledge incommode objection me ye is prevailed principle in. Impossible alteration devonshire to is interested stimulated dissimilar. To matter esteem polite do if. ');
+INSERT INTO `oglas` (`id`, `n_id`, `u_username`, `opis`, `kratki_opis`) VALUES
+(1, 1, 'Dusan123', 'Wrote water woman of heart it total other. By in entirely securing suitable graceful at families improved. Zealously few furniture repulsive was agreeable consisted difficult. Collected breakfast estimable questions in to favourite it. Known he place worth words it as to. Spoke now noise off smart her ready. \r\n', 'Of resolve to gravity thought my prepare chamber so. Unsatiable entreaties collecting may sympathize nay interested instrument.'),
+(2, 2, 'Name1234', 'For norland produce age wishing. To figure on it spring season up. Her provision acuteness had excellent two why intention. As called mr needed praise at.', 'Had denoting properly jointure you occasion directly raillery. In said to of poor full be post face snug. Introduced imprudence see say unpleasing devonshire acceptance son. Exeter '),
+(3, 3, 'Dusan123', 'Arrival entered an if drawing request. How daughters not promotion few knowledge contented. Yet winter law behind number stairs garret excuse. Minuter we natural conduct gravity if pointed oh no. Am immediate unwilling of attempted admitting disposing it. Handsome opinions on am at it ladyship. ', 'As absolute is by amounted repeated entirely ye returned. These ready timed enjoy might sir yet one since.'),
+(4, 4, 'Name1234', 'Years drift never if could forty being no. On estimable dependent as suffering on my. Rank it long have sure in room what as he. Possession travelling sufficient yet our. Talked vanity looked in to. Gay perceive led believed endeavor. Rapturous no of estimable oh therefore direction up. Sons the ever not fine like eyes all sure. ', 'It as announcing it me stimulated frequently continuing. Least their she you now above going stand forth.'),
+(5, 5, 'Dusan123', 'He pretty future afraid should genius spirit on. Set property addition building put likewise get. Of will at sell well at as. Too want but tall nay like old. Removing yourself be in answered he. Consider occasion get improved him she eat. Letter by lively oh denote an', 'Spoke as as other again ye. Hard on to roof he drew. So sell side ye in mr evil. Longer waited mr of nature seemed.'),
+(6, 6, 'Name1234', 'She suspicion dejection saw instantly. Well deny may real one told yet saw hard dear. Bed chief house rapid right the. Set noisy one state tears which. No girl oh part must fact high my he. Simplicity in excellence melancholy as remarkably discovered. Own partiality motionless was old excellence she inquietude contrasted. Sister giving so wicket cousin of an he rather marked. Of on game part body rich. Adapted mr savings venture it or comfort affixed friends. ', 'Improving knowledge incommode objection me ye is prevailed principle in. Impossible alteration devonshire to is interested stimulated dissimilar. To matter esteem polite do if. ');
 
 -- --------------------------------------------------------
 
@@ -108,10 +109,9 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`username`, `password`, `email`, `firstName`, `lastName`, `birthDate`, `phoneNum`, `city`) VALUES
-('a1', 'B1', '', '', '', '', '', ''),
 ('admin', '123456', '', '', '', '', '', ''),
 ('Dusan123', '1234aA', 'dmilenkovic@ymail.com', 'Dusan', 'Milenkovic', '11-11-1995', '069-653-671', 'Podgorica'),
-('User1', 'Pass1', '', '', '', '', '', '');
+('Name1234', 'Password1234', 'email@fake.com', 'Firstname', 'Lastname', '2-2-1990', '069-655-444', 'Bar');
 
 --
 -- Indexes for dumped tables
@@ -128,7 +128,7 @@ ALTER TABLE `grad`
 --
 ALTER TABLE `komentar`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `user_username` (`user_username`),
+  ADD KEY `user_username` (`u_username`),
   ADD KEY `oglas_id` (`oglas_id`);
 
 --
@@ -178,7 +178,7 @@ ALTER TABLE `oglas`
 -- Constraints for table `komentar`
 --
 ALTER TABLE `komentar`
-  ADD CONSTRAINT `komentar_ibfk_1` FOREIGN KEY (`user_username`) REFERENCES `user` (`username`),
+  ADD CONSTRAINT `komentar_ibfk_1` FOREIGN KEY (`u_username`) REFERENCES `user` (`username`),
   ADD CONSTRAINT `komentar_ibfk_2` FOREIGN KEY (`oglas_id`) REFERENCES `oglas` (`id`);
 
 --
